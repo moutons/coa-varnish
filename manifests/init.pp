0e601079ceee19d7731514dc0c531ef37f406e2f
Jerald Sheets <questy@gmail.com>
2014-03-20 17:26:59 -0500

    reverted to old classloader

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,5 @@
 # CoA Varnish Module
 class coa-varnish {
-  class { '::coa-varnish::config': } ->
-  class { '::coa-varnish::service': } ->
-  Class['coa-varnish']
+  include coa-varnish::config
+  include coa-varnish::service
 }
